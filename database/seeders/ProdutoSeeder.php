0e216a661ecc2d07d9408acf6c61c83fd7c3bb0e
andreneves <andr@andr.com.br>
2025-02-17 19:48:54 -0300

    migrate e seed

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('produtos')->insert([
+            'nome' => 'Sapato',
+            'quantidade' => 10,
+            'valor' => 100.10,
+        ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Tênis',
+            'quantidade' => 15,
+            'valor' => 150.25,
+        ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Crocs',
+            'quantidade' => 30,
+            'valor' => 290.71,
+        ]);
+
+    }
+}
